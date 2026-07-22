2642eb9d26aae4cca3c571b9002eac3b842d055b
rafael-vincensi <rafaelvincensi.m@gmail.com>
2026-07-22 14:18:26 -0300

    feat: cria UserRepository

---
+++ src/main/java/com/rafaelvincensi/socialapp/user/UserRepository.java
@@ -0,0 +1,9 @@
+package com.rafaelvincensi.socialapp.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserModel, Long> {
+}
