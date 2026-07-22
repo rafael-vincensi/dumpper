0d248b550edbdcc2ac6ed8e8cf9f069bccda6dce
rafael-vincensi <rafaelvincensi.m@gmail.com>
2026-07-22 15:22:08 -0300

    feat: cria funcoes basicas de MusicController

--- src/main/java/com/rafaelvincensi/socialapp/music/MusicController.java
+++ src/main/java/com/rafaelvincensi/socialapp/music/MusicController.java
@@ -1,4 +1,25 @@
 package com.rafaelvincensi.socialapp.music;
 
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/musics")
 public class MusicController {
+
+    private MusicService musicService;
+
+    public MusicController(MusicService musicService) {
+        this.musicService = musicService;
+    }
+
+    @PostMapping
+    public MusicModel criarMusica(@RequestBody MusicModel musicModel){
+        return musicService.criarMusica(musicModel);
+    }
+
+    @GetMapping("{id}")
+    public MusicModel listarMusicaPorId(@PathVariable Long id){
+        return musicService.listarMusicaPorId(id);
+    }
+
 }
