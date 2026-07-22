cb5c6771e9db6880910bffc416224e1cb1c761ab
rafael-vincensi <rafaelvincensi.m@gmail.com>
2026-07-22 14:10:25 -0300

    fix: adiciona bio para tabela users

---
+++ src/main/java/com/rafaelvincensi/socialapp/user/UserController.java
@@ -0,0 +1,4 @@
+package com.rafaelvincensi.socialapp.user;
+
+public class UserController {
+}
--- src/main/resources/db/migration/V1__create_table_user.sql
+++ src/main/resources/db/migration/V1__create_table_user.sql
@@ -4,6 +4,7 @@ CREATE TABLE users (
     username VARCHAR(50) NOT NULL UNIQUE,
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR(255) NOT NULL,
+    bio VARCHAR(255) NOT NULL,
     profile_picture TEXT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
