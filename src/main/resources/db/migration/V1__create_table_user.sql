b7683d66aba96c74ae2ab67a6120e3f37df051bf
rafael-vincensi <rafaelvincensi.m@gmail.com>
2026-07-22 13:53:43 -0300

    feat: cria schema inicial do banco de dados da tabela users

---
+++ src/main/resources/db/migration/V1__create_table_user.sql
@@ -0,0 +1,9 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    profile_picture TEXT,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
