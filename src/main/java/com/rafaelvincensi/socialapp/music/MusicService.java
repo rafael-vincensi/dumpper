30098f8ea774eeb0a51d4420793a9c250efde64c
rafael-vincensi <rafaelvincensi.m@gmail.com>
2026-07-22 15:17:33 -0300

    feat: cria funcoes de MusicService

--- src/main/java/com/rafaelvincensi/socialapp/music/MusicService.java
+++ src/main/java/com/rafaelvincensi/socialapp/music/MusicService.java
@@ -1,4 +1,25 @@
 package com.rafaelvincensi.socialapp.music;
 
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
 public class MusicService {
+
+    private MusicRepository musicRepository;
+
+    public MusicService(MusicRepository musicRepository) {
+        this.musicRepository = musicRepository;
+    }
+
+    public MusicModel criarMusica(MusicModel musicModel){
+        return musicRepository.save(musicModel);
+    }
+
+    public MusicModel listarMusicaPorId(Long id){
+        Optional<MusicModel> musicPorId = musicRepository.findById(id);
+        return musicPorId.orElse(null);
+    }
+
 }
