10b72ff79d8933fda7408a4df329a8c7b273920e
rafael-vincensi <rafaelvincensi.m@gmail.com>
2026-08-03 19:51:43 -0300

    feat: adiciona metodo de find by artist e find by title no music service

--- socialapp/src/main/java/com/rafaelvincensi/socialapp/music/MusicService.java
+++ socialapp/src/main/java/com/rafaelvincensi/socialapp/music/MusicService.java
@@ -1,20 +1,25 @@
 package com.rafaelvincensi.socialapp.music;
 
 import org.springframework.stereotype.Service;
-
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class MusicService {
 
-    private MusicRepository musicRepository;
+    private final MusicRepository musicRepository;
 
     public MusicService(MusicRepository musicRepository) {
         this.musicRepository = musicRepository;
     }
 
-    public MusicModel criarMusica(MusicModel musicModel){
+    public MusicModel cadastrarMusica(MusicModel musicModel){
         return musicRepository.save(musicModel);
+    }
+
+    public List<MusicModel> listarMusicas(){
+        List<MusicModel> musicModel = musicRepository.findAll();
+        return musicModel;
     }
 
     public MusicModel listarMusicaPorId(Long id){
@@ -22,4 +27,16 @@ public class MusicService {
         return musicPorId.orElse(null);
     }
 
+    public List<MusicModel> findByTitleContainingIgnoreCase(String title) {
+        return musicRepository.findByTitleContainingIgnoreCase(title);
+    }
+
+    public List<MusicModel> findByArtistContainingIgnoreCase (String artist){
+        return musicRepository.findByArtistContainingIgnoreCase(artist);
+    }
+
+    public void deleteMusic(Long id){
+        musicRepository.deleteById(id);
+    }
 }
+
