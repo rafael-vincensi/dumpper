cd76cd4c2308e15b3b7a0eebdfff44a0d5766467
rafael-vincensi <rafaelvincensi.m@gmail.com>
2026-08-03 19:48:57 -0300

    fix: adiciona find by artist e find by title no music repository

--- socialapp/src/main/java/com/rafaelvincensi/socialapp/music/MusicRepository.java
+++ socialapp/src/main/java/com/rafaelvincensi/socialapp/music/MusicRepository.java
@@ -1,6 +1,9 @@
 package com.rafaelvincensi.socialapp.music;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface MusicRepository extends JpaRepository<MusicModel, Long> {
+    List<MusicModel> findByArtistContainingIgnoreCase(String artist);
+    List<MusicModel> findByTitleContainingIgnoreCase(String title);
 }
