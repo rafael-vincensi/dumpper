5312b4fa2b43a7332d55b098846e4ccf4e218300
rafael-vincensi <rafaelvincensi.m@gmail.com>
2026-08-12 21:55:22 -0300

    fix: atualiza user model

--- socialapp/src/main/java/com/rafaelvincensi/socialapp/post/PostRepository.java
+++ socialapp/src/main/java/com/rafaelvincensi/socialapp/post/PostRepository.java
@@ -1,7 +1,6 @@
 package com.rafaelvincensi.socialapp.post;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface PostRepository extends JpaRepository <PostModel, Long> {
