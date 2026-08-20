88c92878f1881352319d580d58452dfc3bf0b1ec
rafael-vincensi <rafaelvincensi.m@gmail.com>
2026-08-19 22:14:38 -0300

    feat(db): atualiza tipo para TEXT de image_url

---
+++ socialapp/src/main/resources/db/V4__alter_image_url_to_text.sql
@@ -0,0 +1,1 @@
+ALTER TABLE tb_post ALTER COLUMN image_url TYPE TEXT;
--- socialapp/src/main/resources/db/migration/V3__create_table_like.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE likes (
-    id SERIAL PRIMARY KEY,
-    user_id BIGINT NOT NULL,
-    post_id BIGINT NOT NULL,
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES users(id),
-    CONSTRAINT fk_post FOREIGN KEY (post_id) REFERENCES tb_post(id),
-    CONSTRAINT unique_user_post UNIQUE (user_id, post_id)
-);
