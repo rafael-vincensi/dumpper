1b142c36f95622177f8cbf4777629632722a5d9d
rafael-vincensi <rafaelvincensi.m@gmail.com>
2026-08-22 14:34:23 -0300

    feat(user): adiciona metodo para busca na search bar

--- socialapp/src/main/java/com/rafaelvincensi/socialapp/user/UserRepository.java
+++ socialapp/src/main/java/com/rafaelvincensi/socialapp/user/UserRepository.java
@@ -3,9 +3,12 @@ package com.rafaelvincensi.socialapp.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, Long> {
     UserModel findByUsername(String username);
     UserModel existsByUsername(String username);
     UserModel findByEmail(String email);
+    List<UserModel> findByUsernameContainingIgnoreCaseOrNameContainingIgnoreCase (String username, String name);
 }
