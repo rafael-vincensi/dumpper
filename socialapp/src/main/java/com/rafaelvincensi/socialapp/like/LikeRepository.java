c6f48a1ab52ba6acb3433eb857707eeb43c9c211
rafael-vincensi <rafaelvincensi.m@gmail.com>
2026-08-24 22:03:21 -0300

    feat(like): cria query de busca se user deu like no post

--- socialapp/src/main/java/com/rafaelvincensi/socialapp/like/LikeRepository.java
+++ socialapp/src/main/java/com/rafaelvincensi/socialapp/like/LikeRepository.java
@@ -2,17 +2,18 @@ package com.rafaelvincensi.socialapp.like;
 
 import com.rafaelvincensi.socialapp.post.PostModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface LikeRepository extends JpaRepository<LikeModel, Long> {
 
-    boolean existsByUserIdAndPostId(Long userId, Long postId);
+    @Query("SELECT COUNT(l) > 0 FROM LikeModel l WHERE l.user.id = :userId AND l.post.id = :postId")
+    boolean existsByUserIdAndPostId(@Param("userId") Long userId, @Param("postId") Long postId);
     Optional<LikeModel> findByUserIdAndPostId(Long userId, Long postId);
-    void deleteByUserIdAndPostId(Long userId, Long postId);
     List<LikeModel> findByPostId(Long postId);
 
 }
