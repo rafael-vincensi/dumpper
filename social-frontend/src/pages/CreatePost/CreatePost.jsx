ca33a6804db5b334f17fb0123f94ef9d3d326924
rafael-vincensi <rafaelvincensi.m@gmail.com>
2026-08-26 20:14:43 -0300

    fix(front): atualiza tela de criar post

--- social-frontend/src/pages/CreatePost/CreatePost.jsx
+++ social-frontend/src/pages/CreatePost/CreatePost.jsx
@@ -1,22 +1,25 @@
 import React, { useState } from 'react';
 import './CreatePost.css';
+import defaultImage from '../../assets/default-image.jpg'
+import previewImage from '../../assets/preview-image.jpg'
+import likeFilled from '../../assets/liked-icon.svg';
+
 
 export default function CreatePost({ onCancel, onSave }) {
   const [description, setDescription] = useState("");
-  const [imageUrl, setImageUrl] = useState(""); // <-- Corrigido o nome da função
+  const [imageUrl, setImageUrl] = useState("");
   const [imagePosition, setImagePosition] = useState("center"); 
   
-  const currentUser = {
-    username: "juniormelansia",
-    avatar: "https://loyolaphoenix.com/wp-content/uploads/2025/03/Courtesy-of-YZY.jpeg"
-  };
+  const usuarioLogadoString = localStorage.getItem("usuarioLogado");
+  const usuarioLogado = usuarioLogadoString ? JSON.parse(usuarioLogadoString) : null;
+  const usuarioLogadoId = usuarioLogado ? usuarioLogado.id : 1;
 
   const handleFileChange = (e) => {
     const file = e.target.files[0];
     if (file) {
       const reader = new FileReader();
       reader.onloadend = () => {
-        setImageUrl(reader.result); // <-- Usando a função correta
+        setImageUrl(reader.result);
       };
       reader.readAsDataURL(file);
     }
@@ -26,7 +29,6 @@ export default function CreatePost({ onCancel, onSave }) {
     e.preventDefault();
     if (!description && !imageUrl) return;
 
-    // Enviando apenas o que o Back-end (PostModel) espera receber
     const newPost = {
       content: description,
       imageUrl: imageUrl
@@ -43,11 +45,7 @@ export default function CreatePost({ onCancel, onSave }) {
 
         <div className="create-form-group">
           <label>choose image</label>
-          <input 
-            type="file" 
-            accept="image/*"
-            onChange={handleFileChange}
-          />
+          <input type="file" accept="image/*" onChange={handleFileChange} />
           <div className="create-position-selector">
             <span>enquadramento:</span>
             <button type="button" onClick={() => setImagePosition("top")}>Topo</button>
@@ -58,12 +56,7 @@ export default function CreatePost({ onCancel, onSave }) {
 
         <div className="create-form-group">
           <label>add description</label>
-          <textarea 
-            placeholder="your description..."
-            value={description}
-            onChange={(e) => setDescription(e.target.value)}
-            rows={3}
-          />
+          <textarea placeholder="your description..." value={description} onChange={(e) => setDescription(e.target.value)} rows={3} />
         </div>
 
         <div className="create-form-actions">
@@ -78,21 +71,21 @@ export default function CreatePost({ onCancel, onSave }) {
           <div className="create-post-header">
             <div className='create-post-profile'>
               <div className='create-post-avatar-wrapper'>
-                <img src={currentUser.avatar} alt="Avatar" />
+                <img src={usuarioLogado?.profilePicture || defaultImage} alt="Avatar" />
               </div>
-              <span className="create-post-author">@{currentUser.username}</span>
+              <span className="create-post-author">@{usuarioLogado.username}</span>
               <p className='create-follow-profile'>follow</p>
             </div>
           </div>
 
           <div className='create-post-content'>
-            <span className="create-post-author">@{currentUser.username}</span>
+            <span className="create-post-author">@{usuarioLogado.username}</span>
             <p className="create-post-text">{description}</p>
           </div>
 
           <div className='create-post-img'>
             <img 
-              src={imageUrl || "https://i.pinimg.com/originals/05/b4/fb/05b4fbc3f169175e6deb97b3977175b6.jpg"} 
+              src={imageUrl || previewImage} 
               alt="Preview" 
               style={{ objectPosition: imagePosition }}
             />
@@ -100,8 +93,8 @@ export default function CreatePost({ onCancel, onSave }) {
 
           <div className="create-post-footer">
             <div className='create-like-container'>
-              <span style={{ fontSize: '18px', marginRight: '4px' }}>❤️</span>
-              <span>999</span>
+              <img src={likeFilled} alt="Like" style={{ width: '18px', marginRight: '4px' }} />
+                <span>999</span>
             </div>
             <span className="create-post-time">1m ago</span>
           </div>
